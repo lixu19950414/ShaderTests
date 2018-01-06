67f263067a605bda1286a0354167477fb7f5fb0d
傲娇的露易丝 <980858415@qq.com>
2018-01-06 10:06:41 +0800

    Add render to skybox editor script

---
+++ Assets/Scripts/Editor/RenderToSkyBox.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class RenderToSkyBox : ScriptableWizard
+{
+    public Transform centerTransform;
+    public Cubemap cubemap;
+
+    void OnWizardUpdate()
+    {
+        helpString = "选择场景的中心点以及目标天空盒";
+        isValid = (centerTransform != null) && (cubemap != null);
+    }
+
+    void OnWizardCreate()
+    {
+        GameObject go = new GameObject("TempCubeMapCamera");
+        go.AddComponent<Camera>();
+        go.transform.position = centerTransform.position;
+        go.GetComponent<Camera>().RenderToCubemap(cubemap);
+        DestroyImmediate(go);
+    }
+
+    [MenuItem("GameObject/渲染到天空盒")]
+    static void RenderCubemap()
+    {
+        DisplayWizard<RenderToSkyBox>("渲染到天空盒", "开始渲染");
+    }
+}
